3a99d004b6d3b0783fafb6d850f4e0aa88ff46d5
littlebithard <lenovolegionvers15@gmail.com>
2025-05-14 23:56:59 +0500

    Create Vertex.java

---
+++ Vertex.java
@@ -0,0 +1,33 @@
+public class Vertex<T> {
+    private T data;
+
+    public Vertex(T data) {
+        this.data = data;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "Vertex{data=" + data + '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Vertex<?> vertex = (Vertex<?>) obj;
+        return data != null ? data.equals(vertex.data) : vertex.data == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return data != null ? data.hashCode() : 0;
+    }
+}
